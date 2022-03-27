b214fa7707507a6727eaa3958f12a6b6f8a3eb47
Trung Nguyen <rezolden@gmail.com>
2022-03-26 21:54:30 -0400

    login

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from database.db import db
 from api.user import user
 
 app = Flask(__name__)
 app.register_blueprint(user, url_prefix='/user')
+
+db.init_app(app)
--- backend/api/user.py
+++ backend/api/user.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 import bcrypt
 import os
 import jwt
+from database.db import TindeeUser
 
 user = Blueprint('user', __name__)
 
@@ -35,16 +36,21 @@ def login():
     
     email = data['email']
     raw_pwd = data['pwd']
-    uuid = 'CHANGE!!'
-    hashed_pwd = 'CHANGE!!'
-    if not bcrypt.checkpw(raw_pwd, hashed_pwd):
-        return 'Wrong password', 400
-    
-    return jwt.encode(
-        {
-            'uuid': uuid,
-            'email': email
-        }, 
-        JWT_SECRET_KEY,
-        algorithm='HS256'
-    )
+    try:
+        uuid = TindeeUser.searchUUID(email)
+        if uuid is None:
+            return 'User not found', 404
+        hashed_pwd = TindeeUser.searchHashpass(uuid)
+        if not bcrypt.checkpw(raw_pwd, hashed_pwd):
+            return 'Wrong password', 400
+        
+        return jwt.encode(
+            {
+                'uuid': uuid,
+                'email': email
+            }, 
+            JWT_SECRET_KEY,
+            algorithm='HS256'
+        )
+    except:
+        return 'We\'re not OK', 500
