942217951301dba9cd3e5e07515460e111168a9b
Trung Nguyen <rezolden@gmail.com>
2022-04-15 15:43:39 -0400

    some minor add

--- backend/api/profile.py
+++ backend/api/profile.py
@@ -4,10 +4,10 @@ from middleware_auth import token_required
 profile = Blueprint('profile', __name__)
 
 def parse_image_from_request(req):
-    
+    return None
 
 @profile.route('/avatar', methods=['POST'])
 @token_required
 def upload_avatar(uuid, email):
     image = parse_image_from_request(request)
-    
+
