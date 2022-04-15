9ee2b25254b689020bbd873896369810ef499e9b
Vivian Luu <nguyenhieuluu@Vivians-MacBook-Air.local>
2022-04-15 19:00:31 -0400

    delete extra file

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,12 +1,12 @@
 import React, { Component, useState } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import 'antd/dist/antd.css';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined, DownloadOutlined } from '@ant-design/icons';
 import './Login.css'
 import logo from "../../assets/hands-helping-solid.svg";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-export default function Login () {
+function LoginForm () {
     const onFinish = (values) => {
       console.log('Success:', values);
     };
@@ -15,81 +15,40 @@ export default function Login () {
       console.log('Failed:', errorInfo);
     };
   
+    
     return (
         <div className='login_page'>
             <div className="login_logo">
-                <img src={logo} /> 
+                <img src={logo} />
                 <span className="title"> tindee </span>
                 <p> where <span className="extra_bold">MENTORS</span> and <span className="extra_bold">MENTEES</span> are meant to <span className="extra_bold">MEET</span> </p>
             </div>
             <div className="login_auth_buttons">
-            <Form
-                name="basic"
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <div className='left'><Form.Label>Email address</Form.Label></div>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
 
-                initialValues={{
-                remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                label="Username"
-                name="username"
-
-                rules={[
-                    {
-                    required: true,
-                    message: 'Please input your username!',
-                    },
-                ]}
-                >
-                <Input className="big_button"/>
-                </Form.Item>
-        
-                <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                    required: true,
-                    message: 'Please input your password!',
-                    },
-                ]}
-                >
-                <Input.Password className="big_button"/>
-                </Form.Item>
-        
-                <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-                >
-                <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-        
-
-
-
-                <Form.Item>
-                
-                            <Button htmlType="submit" type="default" shape="round" className="big_button" ghost size="large">
-                                LOG IN
-                            </Button>
-                            </Form.Item>
-                    <p> By clicking Log In, you agree with our Terms</p>
-                    <p> Learn how we process your data in our Privacy Policy and Cookies Policy </p>
-                
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <div className='left'><Form.Label>Password</Form.Label></div>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant='outline-light' type="submit" className='big_button'>
+                    LOG IN
+                </Button>
             </Form>
             </div>
-      </div>
+            <p> By clicking Log In, you agree with our Terms. </p>
+            <p> Learn how we process your data in our Privacy Policy and Cookies Policy </p>
+            
+    </div>
+
     );
-  };
+};
   
 
+export default LoginForm;
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Login/>, rootElement);
+ReactDOM.render(<LoginForm/>, rootElement);
