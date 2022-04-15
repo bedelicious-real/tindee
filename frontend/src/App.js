3a489c569479817088e7cfb8d574772e328e5399
Hieu114 <biluclac0412@gmail.com>
2022-04-15 15:38:43 -0400

    user_card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login/Login';
+import UserCard from './components/UserCard/UserCard';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <UserCard />
     </div>
   );
 }
