856fd46f8f0d0864fce38e4ce466e5e59fc16f20
Vivian Luu <nguyenhieuluu@Vivians-MacBook-Air.local>
2022-04-17 17:37:57 -0400

    finish static edit mentee profile

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { MentorProfile } from '../../components/UserProfile/index.js';
+import { MentorProfile, MenteeProfile } from '../../components/UserProfile/index.js';
 import './EditProfile.css';
 
 function EditProfile() {
         return (
             <div>
                 <MentorProfile />
+                <MenteeProfile />
             </div>
         );
 }
