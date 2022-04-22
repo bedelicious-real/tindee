1577dbf09a0ebd913a0d89ee1171b4820d8d9d97
Vivian Luu <nguyenhieuluu@Vivians-MacBook-Air.local>
2022-04-22 12:53:52 -0400

    remove name from mentee profile

--- frontend/src/components/UserProfile/MenteeProfile.js
+++ frontend/src/components/UserProfile/MenteeProfile.js
@@ -43,31 +43,6 @@ function MenteeProfile() {
               {...formItemLayout}
               onFinish={onFinish}
             >
-  
-            <Form.Item
-                name="first-name"
-                label="First Name"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your first name!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="last-name"
-                label="Last Name"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your last name!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
             <Form.Item
                 name="organization"
                 label="Organization/Company"
